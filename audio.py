e3dab6d8990c061f6a76834c9b20f8f011463b00
maaaaads <mrth17@student.aau.dk>
2019-04-11 13:37:43 +0200

    Added speech_localization function, and code clean up
    Will recognize the words for the locations, and respond saying that it will find it. If not location word is found, it will say it does not know that location.

--- audio.py
+++ audio.py
@@ -10,43 +10,98 @@ from naoqi import ALModule
 PEPPER_IP="pepper.local"
 PEPPER_PORT=9559
 
-def main():
-	myBroker = ALBroker("myBroker",
-		"0.0.0.0",
-		0,
-		PEPPER_IP,
-		PEPPER_PORT)
-
+dataWord=[]
+asr_speech_localization=empty
 
 # Speech recognition
-class SpeechRecognition:
+class SpeechRecognition(object):
 
-	def speech_recognition(self):
+       def __init__(self):
+              #Services
+              self.memory = session.service("ALMemory")
+              self.tts = session.service("ALTextToSpeech")
+              self.asr = session.service("ALSpeechRecognition")
+              self.proxy = ALProxy("ALMemory", PEPPER_IP, PEPPER_PORT)
+              self.auto_move = session.service("ALAutonomousLife")
 
-		tts=ALProxy("ALTextToSpeech")
+              #Speech recognition setup
+              self.asr.pause(True)
+              self.asr.setVisualExpression(True)
+              self.asr.setAudioExpression(True)
+              self.asr.removeAllContext()
+              self.proxy.subscribeToEvent('WordRecognized', PEPPER_IP, 'wordRecognized')
+              self.asr.setLanguage("English")
+              self.asr.pause(False)
 
-         #Getting the service for speech recognition
-                asr_service = self.service("ALSpeechRecognition")
-                asr_service.setLanguage("English")
+       def speech_recognition(self):
 
-                # Setting the vocabulary
-                vocabulary=["can","you","hear", "me"]
-                asr_service.setVocabulary(vocabulary, False)
+              dataWord=self.proxy.getData("WordRecognized")
+              print("Data: %s" % dataWord)
 
-	
+              # Setting the vocabulary
+              vocabulary=["can you hear me"]
+              self.asr.pause(True)
+              self.asr.setVocabulary(vocabulary, False)
+              self.asr.pause(False)
 
-         #Start the speech recognition engine
-                asr_service.subscribe("Subscribe_Name")
-	        print 'Speech recognition has started'
-	        time.sleep(5)
-         #some code here to run when sentence is heard
-                asr_service.unsubscribe("Subscribe_Name")
-	
-class TextToSpeech:
-	def text_to_speech(self):
-         tts=ALProxy("ALTextToSpeech")
-         tts.say("I am speaking")
+              #Start the speech recognition engine
+              self.asr.subscribe("Subscribe_Name")
+              print("Speech recog is running")
+              time.sleep(10)
+
+              self.asr.pause(False)
+              self.asr.unsubscribe("Subscribe_Name")
+
+              dataWord=self.proxy.getData("WordRecognized")
+              print("Data: %s" % dataWord)
+              self.asr.pause(True)
+              self.asr.removeAllContext()
+
+       def speech_localization(self):
+
+              self.asr.pause(True)
+              self.asr.removeAllContext()
+              vocabulary_localization=["stairs", "bathroom", "exit", "canteen", "elevator"]
+              self.asr.setVocabulary(vocabulary_localization, False)
+              self.asr.pause(False)
+
+              self.asr.subscribe("Speech_Localization")
+              print("Speechrecog is running")
+              time.sleep(5)
+              self.asr.unsubscribe("Speech_Localization")
+              asr_speech_localization=self.proxy.getData("WordRecognized")
+              print("Localization: %s" % asr_speech_localization)
+
+              if asr_speech_localization[0] == 'stairs':
+                     self.tts.say("Ok, I will find the stairs")
+              elif asr_speech_localization[0] == 'bathroom':
+                     self.tts.say("Ok, I will find the bathroom")
+              elif asr_speech_localization[0] == 'exit':
+                     self.tts.say("Ok, I will find the exit")
+              elif asr_speech_localization[0] == 'canteen':
+                     self.tts.say("Ok, I will find the canteen")
+              elif asr_speech_localization[0] == 'elevator':
+                     self.tts.say("Ok, I will find the elevator")
+              else:
+                     self.tts.say("Sorry, I do not know that location")
+
+              self.asr.pause(True)
+              self.asr.removeAllContext()
+
+
+def main():
+       obj=SpeechRecognition()
+       obj.speech_localization()
+       
 
 
 if __name__=="__main__":
-	SpeechRecognition(self)
+       session = qi.Session()
+       try:
+        session.connect("tcp://" + PEPPER_IP + ":" + str(PEPPER_PORT))
+        #app = qi.Application(["SayHi", "--qi-url=" + "tcp://" + PEPPER_IP + ":" + str(PEPPER_PORT)])
+       except RuntimeError:
+        print("wow")
+        sys.exit(1)
+
+       main()
